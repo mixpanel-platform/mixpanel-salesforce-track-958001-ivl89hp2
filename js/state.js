417f77b2848050eb47763ca898c4de87a02417b5
Steven Baum <stevenbaum@berkeley.edu>
2016-12-05 10:36:34 -0800

    Add files via upload

---
+++ js/state.js
@@ -0,0 +1,31 @@
+// App Adoption
+var appAdoptionData = [];
+
+/* 
+	JQL, set appAdoptionData to results,
+	when done run:
+		fillAdoptionTable(appAdoptionData);
+		calculateGrade(appAdoptionData)
+*/
+
+
+// User Leaderboard
+var leaderboardData = [];
+
+/*
+	JQL, set leaderboardData to results,
+	when done run:
+		fillLeaderboardTable(leaderboardData);
+*/
+
+
+// Usage
+var usageData = {};
+
+/*
+	JQL, set usageData to results,
+	when done run:
+		populateUsageTable(usageData);
+
+	ALSO include JQL calls in dropdown onChange logic
+*/
